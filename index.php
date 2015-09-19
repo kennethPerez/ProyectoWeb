98670c8cc624c65a5dce1b8845f01c9e40898f84
kennethPerez <kenn.perez@gmail.com>
2015-09-19 17:44:11 -0600

    Fauricio. Ventana bienvenida modificada. 5:44 p.m.

--- index.php
+++ index.php
@@ -23,27 +23,31 @@
                 </div>
             </div>
             <br>
-            <form class="col-md-5">
-                <div class="col-md-4 col-md-offset-1">
-                    <p>
-                        <label class="label-size">Usuario</label>
-                        <input class="text-box" type="text" name="login-user">
-                    </p>
-                </div>
-                <div class="col-md-4">
-                    <p>
-                        <label class="label-size">Contraseña</label>
-                        <input class="text-box" type="password" name="login-pass">
-                    </p>
-                </div>
-                <br>
-                <div class="col-md-3">
-                    <button class="button be-blue white">Entrar</button>
-                </div>
-                <div class="col-md-6 col-md-offset-4 text-center">
-                    <a>¿Olvidaste tu contraseña?</a>
-                </div>
-            </form>
+            <div class="col-md-5">
+                <form action="php/logInValidate.php" method="POST">
+                    <div class="col-md-4 col-md-offset-1">
+                        <p>
+                            <label class="label-size">Usuario</label>
+                            <input id="box-login-user" class="text-box" type="text" name="login-user">
+                            <h6 id="error-login-user"></h6>
+                        </p>
+                    </div>
+                    <div class="col-md-4">
+                        <p>
+                            <label class="label-size">Contraseña</label>
+                            <input id="box-login-pass" class="text-box" type="password" name="login-pass">
+                            <h6 id="error-login-pass"></h6>
+                        </p>
+                    </div>
+                    <br>
+                    <div class="col-md-3">                        
+                        <input class="button be-blue white" value="Entrar" type="button" onclick="logInValidate();"/>
+                    </div>
+                    <div class="col-md-6 col-md-offset-4 text-center">
+                        <a>¿Olvidaste tu contraseña?</a>
+                    </div>
+                </form>
+            </div>
             <br>
         </div>
         <div class="col-md-12">
@@ -155,33 +159,35 @@
             <!-- INICIO CARRUSEL -->
             <br>
             <div class="col-md-6">
-                <div id="wrapper">
-                    <div id="slider_container_2">
-                        <div id="SliderName_2">
-                            <img src="img/1.jpg" width="100%" height="390" alt="Demo2 first" title="Demo2 first" />
-                            <div class="SliderName_2Description">Featured model: <strong>Charlize Theron</strong></div>
-                            <img src="img/2.jpg" width="100%" height="390" alt="Demo2 second" title="Demo2 second" />
-                            <div class="SliderName_2Description">Featured model: <strong>Charlize Theron</strong></div>
-                            <img src="img/3.jpg" width="100%" height="390" alt="Demo2 third" title="Demo2 third" />
-                            <div class="SliderName_2Description">Featured model: <strong>Charlize Theron</strong></div>
-                            <img src="img/4.jpg" width="100%" height="390" alt="Demo2 fourth" title="Demo2 fourth" />
-                            <div class="SliderName_2Description">Featured model: <strong>Charlize Theron</strong></div>
+                <div class="col-md-12">
+                    <div class="col-md-12" id="wrapper">
+                        <div id="slider_container_2">
+                            <div id="SliderName_2">
+                                <img src="img/1.jpg" width="100%" height="390" alt="Demo2 first" title="Demo2 first" />
+                                <div class="SliderName_2Description">Featured model: <strong>Charlize Theron</strong></div>
+                                <img src="img/2.jpg" width="100%" height="390" alt="Demo2 second" title="Demo2 second" />
+                                <div class="SliderName_2Description">Featured model: <strong>Charlize Theron</strong></div>
+                                <img src="img/3.jpg" width="100%" height="390" alt="Demo2 third" title="Demo2 third" />
+                                <div class="SliderName_2Description">Featured model: <strong>Charlize Theron</strong></div>
+                                <img src="img/4.jpg" width="100%" height="390" alt="Demo2 fourth" title="Demo2 fourth" />
+                                <div class="SliderName_2Description">Featured model: <strong>Charlize Theron</strong></div>
+                            </div>
+                            <div class="c"></div>
+                            <center><div id="SliderNameNavigation_2"></div></center>
+                            <div class="c"></div>
+                            <script type="text/javascript">
+                                effectsDemo2 = 'rain, stairs, fade';
+                                var demoSlider_2 = Sliderman.slider({container: 'SliderName_2', width: "100%", height: 390, effects: effectsDemo2,
+                                    display: {
+                                        autoplay: 3000,
+                                        loading: {background: '#000000', opacity: 0.5, image: 'img/loading.gif'},
+                                        buttons: {hide: true, opacity: 1, prev: {className: 'SliderNamePrev_2', label: ''}, next: {className: 'SliderNameNext_2', label: ''}},
+                                        description: {hide: true, background: '#000000', opacity: 0.4, height: 50, position: 'bottom'},
+                                        navigation: {container: 'SliderNameNavigation_2', label: '<img src="img/clear.gif" />'}
+                                    }
+                                });
+                            </script>
                         </div>
-                        <div class="c"></div>
-                        <center><div id="SliderNameNavigation_2"></div></center>
-                        <div class="c"></div>
-                        <script type="text/javascript">
-                            effectsDemo2 = 'rain, stairs, fade';
-                            var demoSlider_2 = Sliderman.slider({container: 'SliderName_2', width: "100%", height: 390, effects: effectsDemo2,
-                                display: {
-                                    autoplay: 3000,
-                                    loading: {background: '#000000', opacity: 0.5, image: 'img/loading.gif'},
-                                    buttons: {hide: true, opacity: 1, prev: {className: 'SliderNamePrev_2', label: ''}, next: {className: 'SliderNameNext_2', label: ''}},
-                                    description: {hide: true, background: '#000000', opacity: 0.4, height: 50, position: 'bottom'},
-                                    navigation: {container: 'SliderNameNavigation_2', label: '<img src="img/clear.gif" />'}
-                                }
-                            });
-                        </script>
                     </div>
                 </div>
                 <br><br>
