10770bb2ef70470b728da6a14c2e9fe9ff967366
kennethPerez <kenn.perez@gmail.com>
2015-09-20 00:53:20 -0600

    Jose Al archivo crearForo 12:52 pm

---
+++ vistas/crearForo.php
@@ -0,0 +1,26 @@
+<div class=" col-md-12 crearForo">
+    
+    <p> 
+        <label> Nombre del foro </label>
+        <input type="text">
+    </p>
+    
+    <p> 
+        <label> Descripción </label>
+        <textarea> </textarea>
+    </p>
+    
+    <p> 
+        <label> Palabras clave </label>
+        <input type="text">
+    </p>
+    
+    <p> 
+        <br>
+        <br> 
+        <input type="submit" value="Crear foro">
+    </p>
+    
+    
+</div>
+
