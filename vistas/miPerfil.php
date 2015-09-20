c29fe7134dec315b7bd95acfe7f84a773f1014ab
kennethPerez <kenn.perez@gmail.com>
2015-09-20 00:53:43 -0600

    Jose Al archivo miPerfil 12:52 pm

---
+++ vistas/miPerfil.php
@@ -0,0 +1,70 @@
+<div class=" col-md-12 miPerfil">
+    
+    <div class="col-md-4"> 
+        
+        <p> <div> Foto </div> </p>
+        <p> <button> Seleccionar nueva foto </button> </p>
+        
+    </div>
+    
+    <div class="col-md-4"> 
+        
+        <p> 
+            <label> Nombre </label>
+            <input type="text">
+        </p>
+        
+        <p> 
+            <label> Apellidos </label> 
+            <input type="text">
+        </p>
+        
+        <p> 
+            <label> Usuario </label> 
+            <input type="text">
+        </p>
+        
+        <p> 
+            <label> Fecha ingreso al TEC </label> 
+            <input type="date">
+        </p>
+        
+    </div>
+    
+    <div class="col-md-4"> 
+        
+        <p> 
+            <label> Sexo </label> 
+            <input type="radio" name="sexo">
+            <label> Masculino </label>
+            <input type="radio" name="sexo">
+            <label> Femenino </label>
+        </p>
+        
+        <p> 
+            <label> Pregunta de seguridad </label> 
+            <select>
+                <option>¿Mi primera mascota?</option>
+                <option>¿Lugar de nacimiento de mi madre?</option>
+                <option>¿Profesora de primaria?</option>
+                <option>¿Canción favorita?</option>
+                <option>¿Marca de ropa favorita?</option>
+            </select>
+        </p>
+        
+        <p> 
+            <label> Respuesta </label> 
+            <input type="text">
+        </p>
+        
+        <p> 
+            <br>
+            <br>
+            <br>
+            <input type="submit" value="Aceptar cambios">
+        </p>
+        
+    </div>
+    
+    
+</div>
