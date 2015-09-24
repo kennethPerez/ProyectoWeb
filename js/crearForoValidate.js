237899ff5f52bbf2e6390bb01b49ca33ef5ced61
kennethPerez <kenn.perez@gmail.com>
2015-09-24 00:02:19 -0600

    Fauricio. Crear foro. 11:11 p.m.

--- js/crearForoValidate.js
+++ js/crearForoValidate.js
@@ -54,23 +54,11 @@ function faroValidate() {
 
             if(ok)
             {
-               var name = $("#box-name").val();
-               var last_name = $("#box-last-name").val();
-               var email = $("#box-email").val();
+               var name = $("#box-name-forum").val();
+               var description = $("#box-description-forum").val();
                
-               var admission_date = $("#box-admission-date").val();
-               
-               var user = $("#box-user").val();
-               var pass = CryptoJS.MD5($("#box-pass").val()); 
-               
-               var security_answer = CryptoJS.MD5($("#box-security-answer").val());   
-               
-               var security_question = $("#select-question").val();
-               var sex = $("#radio-sex").val();
-               
-               location.href = "/php/registrationNewUser.php?name="+name+"&last_name="+last_name+"&email="+email+"&admission_date="+admission_date+"&user="+user+"&pass="+pass+"&security_answer="+security_answer+"&security_question="+security_question+"&sex="+sex;
+               location.href = "/php/insertForo.php?name="+name+"&description="+description;
             }
-            $('#status').html('');
         },
         error: function(jqXHR, estado, error) 
         {
