0ca932e0ba03639638601e92768b5b15cf7cf65a
kennethPerez <kenn.perez@gmail.com>
2015-09-23 21:57:56 -0600

    Jose A los archivos vistas y misEstilos, crearForo 5:15 pm

--- vistas/crearForo.php
+++ vistas/crearForo.php
@@ -1,24 +1,27 @@
 <div class="col-md-12"> 
     <h2>Crear foro</h2>
 </div>
-<div class="col-md-12 crearForo">
-    <div class="col-md-6 col-md-offset-3">
-        <div col-md-12>
-            <p> 
-                <label class="label-size">Nombre</label>
-                <input id="box-name-forum" class="text-box" type="text" name="name-forum">
-                <h6 id="error-name-forum"></h6>
-            </p>
-        </div>
-        <div col-md-12>
-            <p> 
-                <label class="label-size">Descripción</label>
-                <textarea id="box-description-forum" class="text-area" name="description-forum"></textarea>
-                <h6 id="error-description-forum"></h6>
-            </p>
-        </div>
-        <div class="col-md-4 col-md-offset-4">
-            <input type="submit" class="button be-green white" value="Crear">
+
+<form id ="crearForo-form" action="/php/createForum.php" method="POST">
+    <div class="col-md-12 crearForo">
+        <div class="col-md-6 col-md-offset-3">
+            <div col-md-12>
+                <p> 
+                    <label class="label-size">Nombre</label>
+                    <input id="box-name-forum" class="text-box" type="text" name="name-forum">
+                    <h6 id="error-name-forum"></h6>
+                </p>
+            </div>
+            <div col-md-12>
+                <p> 
+                    <label class="label-size">Descripción</label>
+                    <textarea id="box-description-forum" class="text-area" name="description-forum"></textarea>
+                    <h6 id="error-description-forum"></h6>
+                </p>
+            </div>
+            <div class="col-md-4 col-md-offset-4">
+                <input type="submit" class="button be-green white" onclick="foroValidate()" value="Crear">
+            </div>
         </div>
     </div>
-</div>
+</form>
