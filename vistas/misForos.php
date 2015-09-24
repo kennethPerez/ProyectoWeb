d09caf90e1fce7a7e6658b0a34ea5b4365d9220d
kennethPerez <kenn.perez@gmail.com>
2015-09-23 21:58:33 -0600

    Jose A los archivos vistas y misEstilos, crearForo 5:15 pm

--- vistas/misForos.php
+++ vistas/misForos.php
@@ -1,3 +1,21 @@
-<div class="col-md-4 misForos">
+<div class="col-md-12 misForos">
+    
+    <h2> Mis foros </h2>
+    <div class="col-md-4 listaForos">
+        <label> <a> Foro1 </a> </label>
+        <label> <a> Foro2 </a> </label>
+        <label> <a> Foro3 </a> </label>
+        <label> <a> Foro4 </a> </label>
+        <label> <a> Foro5 </a> </label>
+        <label> <a> Foro6 </a> </label>
+        <label> <a> Foro7 </a> </label>
+        <label> <a> Foro8 </a> </label>
+        <label> <a> Foro9 </a> </label>
+        <label> <a> Foro10 </a> </label>
+    </div>
+    
+    <div class="col-md-8 foroActual">
+        Foro seleccionado
+    </div>
     
 </div>
