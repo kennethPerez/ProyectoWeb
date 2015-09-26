cd79218174fbeadee23d2d53c3d932c772378df6
kennethPerez <kenn.perez@gmail.com>
2015-09-26 02:45:39 -0600

    Jose Archivo crearForoValidate.php 5:15 pm

--- php/crearForoValidate.php
+++ php/crearForoValidate.php
@@ -5,16 +5,29 @@
     
     $name = $_REQUEST['name-forum'];
     $description = $_REQUEST['description-forum'];           
-    
+
     $array_data[] = nameValidate($name);
-    $array_data[] = descriptionValidate($description);      
+    $array_data[] = descriptionValidate($description);    
+    
+    if($array_data[0]["state"] === "Correcto" and $array_data[1]["state"] === "Correcto")
+    {
+        if (isset($_SESSION["rowUser"]))
+        {
+            $rowUser = $_SESSION["rowUser"];
+            $strconn="host=localhost port=5432 dbname=gitbook user=postgres password=12345";    
+            $conn=pg_connect($strconn);  
+            $query = "INSERT INTO foros(idpersona,titulo,descripcion)";
+            $query .=" values('$rowUser[0]','$name','$description')";
+            $result = pg_query($conn,$query);
+        }
+    }
         
     echo json_encode($array_data);
     
     function nameValidate($name)
     {
         if(strlen($name) >= 5)
-        {                     
+        {         
             return array(
                 'state' => "Correcto",
                 'box' => "#box-name-forum"
