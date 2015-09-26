01be9fa6a34120bba5abc20e0cd47658f8937f44
kennethPerez <kenn.perez@gmail.com>
2015-09-26 02:50:00 -0600

    Jose Archivo misPublicaciones.php 5:15 pm

--- vistas/misPublicaciones.php
+++ vistas/misPublicaciones.php
@@ -4,5 +4,11 @@
 <div class="col-md-12 misPublicaciones">
     <div class="col-md-6 col-md-offset-3">
         <label class="label-size text-center">No hay publicaciones</label>
+        <?php
+            include '/var/www/proyectoWeb/trunk/php/cargarPublForos.php';
+            $con = new conexion();
+            $con->cargarMisPublicaciones();
+            
+        ?>
     </div>
 </div>
