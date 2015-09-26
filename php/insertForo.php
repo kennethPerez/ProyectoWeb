bce6a86197f8f8c3b11bc9b98ad7f16acfc35c65
kennethPerez <kenn.perez@gmail.com>
2015-09-26 02:46:55 -0600

    Jose Archivo insertForo.php 5:15 pm

--- php/insertForo.php
+++ php/insertForo.php
@@ -10,7 +10,7 @@
         $strconn="host=localhost port=5432 dbname=gitbook user=postgres password=12345";    
         $conn=pg_connect($strconn);  
         $query = "INSERT INTO foros(idpersona,titulo,descripcion)";
-        $query .=" values('$rowUser[0]','titulo','descripcion')";
+        $query .=" values('$rowUser[0]','$titulo','$descripcion')";
         $result = pg_query($conn,$query);
     }
     
