6047ef64ac14a65ce36a1055ef5ffb103921664f
kennethPerez <kenn.perez@gmail.com>
2015-09-26 02:48:16 -0600

    Jose Archivo misForos.php 5:15 pm

--- vistas/misForos.php
+++ vistas/misForos.php
@@ -3,20 +3,15 @@
 </div>
 <div class="col-md-12 misForos">
     <div class="col-md-4 listaForos">
-        <label> <a> Foro1 </a> </label>
-        <label> <a> Foro2 </a> </label>
-        <label> <a> Foro3 </a> </label>
-        <label> <a> Foro4 </a> </label>
-        <label> <a> Foro5 </a> </label>
-        <label> <a> Foro6 </a> </label>
-        <label> <a> Foro7 </a> </label>
-        <label> <a> Foro8 </a> </label>
-        <label> <a> Foro9 </a> </label>
-        <label> <a> Foro10 </a> </label>
+        <?php
+            include '/var/www/proyectoWeb/trunk/php/cargarPublForos.php';
+            $con = new conexion();
+            $con->cargarMisForos();
+        ?>
     </div>
     
     <div class="col-md-8 foroActual">
-        Foro seleccionado
+        <label id="foro-actual"> </label>
     </div>
     
 </div>
