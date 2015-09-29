ec896795ea834a94f8eecf85a84c9108c52d0f36
kennethPerez <kenn.perez@gmail.com>
2015-09-28 23:27:11 -0600

    Jose Archivo vistaPrincipal.php 5:15 pm

--- vistas/forosActivos.php
+++ vistas/forosActivos.php
@@ -3,21 +3,29 @@
 </div>
 <div class="col-md-12 misForos">
     <div class="col-md-3 listaForos">
-        <label> <a> Foro1 </a> </label>
-        <label> <a> Foro2 </a> </label>
-        <label> <a> Foro3 </a> </label>
-        <label> <a> Foro4 </a> </label>
-        <label> <a> Foro5 </a> </label>
-        <label> <a> Foro6 </a> </label>
-        <label> <a> Foro7 </a> </label>
-        <label> <a> Foro8 </a> </label>
-        <label> <a> Foro9 </a> </label>
-        <label> <a> Foro10 </a> </label>
+        <?php
+            include '/var/www/proyectoWeb/trunk/php/cargarPublForos.php';
+            $con = new conexion();
+            $con->cargarForosActivos();
+        ?>
     </div>
-    
-    <div class="col-md-9 foroActual">
-        Foro seleccionado
-    </div>
+    <form id="form-comentario-foro" action="/php/agregarComentarioForoValidate.php" method="POST">
+        <div class="col-md-9 foroActual">
+            <label id="foro-activo-nombre"> </label>
+            <label id="foro-activo-descripcion"> </label>
+            <label id="foro-activo-idforo"> </label>
+            <input type="text" id="foro-activo-identificador" name="foro-activo-identificador" style="display: none">
+            <br>
+            <div id="comments">
+            </div>
+            <label id="label-comentar-foro" style="display: none"> ¡Haz tu comentario en el foro! </label>
+            <textarea id="text-comentar-foro" name="text-comentar-foro" style="display: none"> </textarea>
+            <label id="error-comentario-foro"> </label>
+            <br>
+            <input type="button" class="button be-green white" id="btn-comentar-foro" onclick="agregarComentario()" style="display: none" value="Comentar">
+            <label id="exito-comentario"> </label>
+        </div>
+    </form>
     
 </div>
 
