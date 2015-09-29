501edbf64e3fbcdc848cb47d94a14d606b9fcf0b
kennethPerez <kenn.perez@gmail.com>
2015-09-28 23:26:42 -0600

    Jose Archivo vistaPrincipal.php 5:15 pm

--- php/cargarPublForos.php
+++ php/cargarPublForos.php
@@ -1,9 +1,12 @@
 <?php
-        
+session_start();
+
     class conexion{
+        
+
         function cargarMisPublicaciones()
         {
-            session_start();
+            
             if (isset($_SESSION["rowUser"]))
             {
                 $rowUser = $_SESSION["rowUser"];
@@ -24,9 +27,6 @@
         
         function cargarMisForos()
         {
-            session_start();
-            
-            $array = array();
             if (isset($_SESSION["rowUser"]))
             {
                 $rowUser = $_SESSION["rowUser"];
@@ -34,11 +34,11 @@
                 $conn=pg_connect($strconn);  
                 $query = "SELECT titulo,descripcion FROM foros WHERE foros.idpersona = '$rowUser[0]'";
                 $result = pg_query($conn,$query);
+                
 
                 while($fila = pg_fetch_array($result))
                 {
                     echo "<label> <a onclick='mostrarDescripForo(\"$fila[descripcion]\")'> $fila[titulo] </a> </label>";
-                    $array[] = $fila;
                 }
                 
                 //$a = count($array);
@@ -46,6 +46,49 @@
             }
 
         }
+        
+        function cargarForosActivos()
+        {
+            if (isset($_SESSION["rowUser"]))
+            {
+                $rowUser = $_SESSION["rowUser"];
+                $strconn="host=localhost port=5432 dbname=gitbook user=postgres password=12345";    
+                $conn=pg_connect($strconn);  
+                                
+                $query = "select nombre,titulo,idforo,descripcion from personas inner join foros on (personas.idpersona = foros.idpersona)";
+                $result = pg_query($conn,$query);
+
+                while($fila = pg_fetch_array($result))
+                {
+                    echo "<label> <a onclick='mostrarInfoForos(\"$fila[nombre]\",\"$fila[descripcion]\",\"$fila[idforo]\")'> $fila[titulo] </a> </label>";
+                }
+                
+                
+                //$a = count($array);
+                //echo json_encode($array);
+                //cambiar el diseño de mostrar los foros
+                //en una consulta la informacion del foro
+                //en otra todos los comentarios
+            }
+        }
+        
+        /*function cargarComentariosDeForo($IDforo)
+        {
+            if (isset($_SESSION["rowUser"]))
+            {
+                $rowUser = $_SESSION["rowUser"];
+                $strconn="host=localhost port=5432 dbname=gitbook user=postgres password=12345";    
+                $conn=pg_connect($strconn);  
+                $query = "SELECT comentario FROM comentarios WHERE comentarios.idforo = $IDforo";
+                $result = pg_query($conn,$query);
+
+                while($fila = pg_fetch_array($result))
+                {
+                    echo "<div> <h4> <strong> $fila[comentario] </strong> <h4> </div>";
+                    echo "<br>";
+                }
+            }
+        }*/
         
     }
 
