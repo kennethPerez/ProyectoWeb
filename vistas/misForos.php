40ab4ca215b6eccd6d827b4ac6d61fc51c6d8591
kennethPerez <kenn.perez@gmail.com>
2015-09-29 13:59:25 -0600

    Jose Archivo vistaPrincipal.php 5:15 pm

--- vistas/misForos.php
+++ vistas/misForos.php
@@ -3,11 +3,6 @@
 </div>
 <div class="col-md-12 misForos">
     <div class="col-md-4 listaForos">
-        <?php
-            include '/var/www/proyectoWeb/trunk/php/cargarPublForos.php';
-            $con = new conexion();
-            $con->cargarMisForos();
-        ?>
     </div>
     
     <div class="col-md-8 foroActual">
