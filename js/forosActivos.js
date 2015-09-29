30adcb7a8672ae166569e5fa0d8113c3e272b69d
kennethPerez <kenn.perez@gmail.com>
2015-09-29 13:55:54 -0600

    Jose Archivo vistaPrincipal.php 5:15 pm

---
+++ js/forosActivos.js
@@ -0,0 +1,31 @@
+function getForosActivos()
+{
+    $(".listaForos").empty();
+    carga('forosActivos.php','cuerpo');
+    var peticion = obtenerXHR(); 
+    peticion.open("GET", "/php/cargarForosActivos.php", true);
+    peticion.onreadystatechange = function() 
+    {
+        if (peticion.readyState === 4 && peticion.status === 200) // Petición completada
+        {
+            var respuestaJSON = peticion.responseText;
+            var json_foros = eval("("+respuestaJSON+")"); // Se evalua la respuesta del JSON
+            
+            for(var i=0; i<json_foros.length; i++) 
+            {
+                var label = document.createElement("label");
+                var a = document.createElement("a");
+                a.setAttribute("onclick", "mostrarInfoForos('"+json_foros[i]['nombre']+"','"+json_foros[i]['descripcion']+"',"+json_foros[i]['idforo']+")");
+                a.appendChild(document.createTextNode(json_foros[i]['titulo']));
+                
+                label.appendChild(a);
+                $(".listaForos").append(label);
+            }
+        }    
+        else // Petición no completada
+        {
+            console.log("No completada | Estado de la petición: " + peticion.readyState); 
+        }
+    };
+    peticion.send(null);     
+}
